22492cd059bf519986900d18b952aab76b8d6e0e
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2022-06-20 16:29:50 +0700

    soal 7

---
+++ soal7(ValidAnagram).py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/valid-anagram/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        h = {}
+        for i in s:
+            h[i] = h.get(i, 0) + 1
+        for j in t:
+            if h.get(j, 0) == 0:
+                return False
+            else:
+                h[j] -= 1
+        for k in h:
+            if h[k] >= 1:
+                return False
+        return True
