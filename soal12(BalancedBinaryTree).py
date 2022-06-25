b9b6ea9c8a014e1acd9e612e57a3da908d7eff0e
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2022-06-25 13:05:56 +0700

    soal 12

---
+++ soal12(BalancedBinaryTree).py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/balanced-binary-tree/
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        ans = self.helper(root)
+        return ans != -1
+    def helper(self, root):
+        if not root:
+            return 0
+        l = self.helper(root.left)
+        r = self.helper(root.right)
+        if l == -1:
+            return -1
+        if r == -1:
+            return -1
+        if abs(r-l) > 1:
+            return -1
+        else:
+            return 1 + max(l, r)
