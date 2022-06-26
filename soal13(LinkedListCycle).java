cbf06d112145b9548ea66234875e8b62e3a34aac
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2022-06-26 12:47:16 +0700

    soal 13

---
+++ soal13(LinkedListCycle).java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/linked-list-cycle/
+// https://medium.com/@nisargdevdhar/leetcode-solution-72406f05b78a
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
