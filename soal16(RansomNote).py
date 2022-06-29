a239428078c41dab021c034d56794e4986fce28b
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2022-06-29 07:07:09 +0700

    soal 16

---
+++ soal16(RansomNote).py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/ransom-note/
+
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        h = [0] * 26
+        for i in magazine:
+            h[ord(i)-ord('a')] = h[ord(i)-ord('a')] + 1
+        for j in ransomNote:
+            if h[ord(j)-ord('a')] <= 0:
+                return False
+            h[ord(j)-ord('a')] -= 1
+        return True
