c1baad86814ca2391f1213d03e1d7426d3044a88
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2022-07-04 10:01:10 +0700

    soal 20

---
+++ soal20(ReverseLinkedList).py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/reverse-linked-list/
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return self.helper(head, None)
+    def helper(self, curr, prev):
+        if curr:
+            temp = curr.next
+            curr.next = prev
+            return self.helper(temp, curr)
+        return prev
