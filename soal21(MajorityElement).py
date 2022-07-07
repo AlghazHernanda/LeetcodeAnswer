83affbc21f1d990893445530ea78193e8edc3895
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2022-07-07 14:55:11 +0700

    soal 21

---
+++ soal21(MajorityElement).py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/majority-element/submissions/
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        return self.helper(nums, 0, len(nums)-1)
+    def helper(self, nums, l, r):
+        if l == r:
+            return nums[l]
+        mid = (l+r) // 2
+        lm = self.helper(nums, l, mid)
+        rm = self.helper(nums, mid+1, r)
+        if lm == rm:
+            return lm
+        else:
+            lc = sum([1 for i in range(l, r+1) if nums[i] == lm])
+            rc = sum([1 for i in range(l, r+1) if nums[i] == rm])
+            if lc > rc:
+                return lm
+            return rm
