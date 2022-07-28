5dbbdac7cec8da6fbc2a07b944b0c542079f7809
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2022-07-28 21:31:52 +0700

    soal 32

---
+++ soal32(BinaryTreeLevelOrderTraversal).py
@@ -0,0 +1,27 @@
+#https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+
+#  Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        s = deque([root])
+        ans = []
+        while s:
+            l = len(s)
+            temp = []
+            for i in range(l):
+                c = s.popleft()
+                temp.append(c.val)
+                if c.left:
+                    s.append(c.left)
+                if c.right:
+                    s.append(c.right)
+            ans.append(temp)
+        return ans
