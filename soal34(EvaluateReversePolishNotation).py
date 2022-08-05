5462b31eb2a7965712c437a1ebb79beeaccb375d
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2022-08-05 17:34:01 +0700

    soal 34

---
+++ soal34(EvaluateReversePolishNotation).py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        d = []
+        for t in tokens:
+            if t == '+':
+                d.append(d.pop() + d.pop())
+            elif t == '-':
+                a, b = d.pop(), d.pop()
+                d.append(b-a)
+            elif t == '/':
+                a, b = d.pop(), d.pop()
+                d.append(int(float(b)/a))
+            elif t == '*':
+                d.append(d.pop() * d.pop())
+            else:
+                d.append(int(t))
+        return d[0]
