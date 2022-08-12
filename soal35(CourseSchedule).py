7ad473af386edc304d717e05483c19bcfec4bede
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2022-08-12 18:40:56 +0700

    soal 35

---
+++ soal35(CourseSchedule).py
@@ -0,0 +1,46 @@
+# https://medium.com/codex/solving-graph-problems-course-schedule-374ead04cfcd
+# https://leetcode.com/problems/course-schedule/
+
+from collections import deque 
+
+class Solution:
+       
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        if len(prerequisites) == 0:
+            return True
+        
+        graph = [[] for _ in range(numCourses)]
+        
+        indegree = [0] * numCourses
+        
+        for a, b in prerequisites:
+            graph[b].append(a)
+            indegree[a] += 1
+            
+        
+        queue = deque()
+        
+        for i in range(0, len(indegree)):
+            if indegree[i] == 0:
+                queue.append(i)
+        
+        count = 0 
+
+        while len(queue) > 0:
+            current = queue.popleft()
+            
+            if indegree[current] == 0:
+                count += 1
+                
+            if not graph[current]:
+                print(f'{current} doesnt exit in {graph}')
+                continue
+                        
+            for neighbour in graph[current]:
+                indegree[neighbour] -= 1
+                if indegree[neighbour] == 0:
+                    queue.append(neighbour)
+                
+        
+        return True if count == numCourses else False
