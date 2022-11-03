283a81288200285601fd7685fadfea80ee711a6e
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2022-11-03 16:56:15 +0700

    change code

--- soal45(Lowest CommonAncestorOfABinaryTree).java
+++ soal45(Lowest CommonAncestorOfABinaryTree).java
@@ -9,8 +9,14 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+
 class Solution {
-    
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null) {
             return null;
