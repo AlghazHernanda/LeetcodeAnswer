fcf2c0c0ae2c6fd282ab160832e46b602559d761
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2022-11-06 15:45:43 +0700

    update answer

--- soal45(Lowest CommonAncestorOfABinaryTree).java
+++ soal45(Lowest CommonAncestorOfABinaryTree).java
@@ -9,12 +9,6 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
- public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
