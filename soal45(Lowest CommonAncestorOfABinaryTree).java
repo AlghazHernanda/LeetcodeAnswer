46af5d40e2c04a0b3565a82338423adf97777514
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2022-12-14 23:45:58 +0700

    postponed code

--- soal45(Lowest CommonAncestorOfABinaryTree).java
+++ soal45(Lowest CommonAncestorOfABinaryTree).java
@@ -11,24 +11,24 @@
  */
 
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) {
-            return null;
-        }
-        if (p.val == root.val || q.val == root.val) {
-            return root;
-        }
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if (left != null && right != null) {
-            return root;
-        }
-        if (left != null) {
-            return left;
-        } else if (right != null) {
-            return right;
-        } else {
-            return null;
-        }
-    }
+    // public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    //     if (root == null) {
+    //         return null;
+    //     }
+    //     if (p.val == root.val || q.val == root.val) {
+    //         return root;
+    //     }
+    //     TreeNode left = lowestCommonAncestor(root.left, p, q);
+    //     TreeNode right = lowestCommonAncestor(root.right, p, q);
+    //     if (left != null && right != null) {
+    //         return root;
+    //     }
+    //     if (left != null) {
+    //         return left;
+    //     } else if (right != null) {
+    //         return right;
+    //     } else {
+    //         return null;
+    //     }
+    // }
 }
