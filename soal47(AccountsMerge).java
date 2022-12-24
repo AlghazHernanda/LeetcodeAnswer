01db4a4f88395f94402b3cd149d4f220795a0d33
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2022-12-24 17:13:32 +0700

    add explanantion

--- soal47(AccountsMerge).java
+++ soal47(AccountsMerge).java
@@ -7,7 +7,7 @@ class Solution {
         Map<String, String> owner = new HashMap<>();    // key: e-mail value: name -> records which user it belongs to.
         Map<String, String> parent = new HashMap<>();   // key: email value: email
         Map<String, TreeSet<String>> uf = new HashMap<>();
-        // Step 1: Initialization
+        // Step 1: inisiasi
         for(List<String> account : accounts){
             for(int i = 1; i < account.size(); i++){
                 parent.put(account.get(i), account.get(i)); // each e-mails was initialized to point to themselves.
@@ -21,19 +21,19 @@ class Solution {
                 parent.put(find(account.get(i), parent), root); // find root for rest e-mails and merge them to the root.
             }
         }
-        // Step 3: Add to uf.(Actually merge different lines)
+        // Step 3: tambahkan uf.(Actually merge different lines)
         for(List<String> account : accounts){
             String root = find(account.get(1), parent); //// find the root e-mail for the first email.
             if(!uf.containsKey(root)) uf.put(root, new TreeSet<String>());
             for(int i = 1; i < account.size(); i++)
                 uf.get(root).add(account.get(i)); // Add all emails in this account to uf.
         }
-        // Step 4: Create result List.
+        // Step 4: buat hasil List.
         List<List<String>> result = new ArrayList<>();
         for(String key : uf.keySet()){
             List<String> temp = new ArrayList<>();
-            temp.add(owner.get(uf.get(key).first()));   // first add the username.
-            temp.addAll(uf.get(key)); // Add all emails to the list.
+            temp.add(owner.get(uf.get(key).first()));   // pertama, tambahkan username.
+            temp.addAll(uf.get(key)); // tambahkan semua email ke list.
             result.add(temp);
         }
         return result;
