1b2288db8597021481c033620cd6a45a9c68208b
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2023-01-27 03:16:15 +0700

    soal 33

--- soal33(CloneGraph).py
+++ soal33(CloneGraph).py
@@ -9,14 +9,14 @@ class Solution:
         return new_root
         
     def rBfs(self, deq, seen, lut):
-        # for i in range(len(deq)):
-        #     new_val = lut[id(deq[0])]
-        #     for j in deq[0].neighbors:
-        #         if id(j) not in lut:
-        #             lut[id(j)] = Node(j.val, [])
-        #         new_val.neighbors.append(lut[id(j)])
-        #         if id(j) not in seen: 
-        #             deq.append(j)
-        #             seen.add(id(j))
-        #     deq.popleft()
+        for i in range(len(deq)):
+            new_val = lut[id(deq[0])]
+            for j in deq[0].neighbors:
+                if id(j) not in lut:
+                    lut[id(j)] = Node(j.val, [])
+                new_val.neighbors.append(lut[id(j)])
+                if id(j) not in seen: 
+                    deq.append(j)
+                    seen.add(id(j))
+            deq.popleft()
         if deq: self.rBfs(deq, seen, lut)
