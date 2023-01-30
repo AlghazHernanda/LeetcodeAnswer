16a4f122270e1a6e6593454bf1b5cf19d72bf981
Mohamad Alghaz Hernanda <74697091+AlghazHernanda@users.noreply.github.com>
2023-01-30 17:39:53 +0700

    update treenode

--- soal32(BinaryTreeLevelOrderTraversal).py
+++ soal32(BinaryTreeLevelOrderTraversal).py
@@ -2,11 +2,11 @@
 
 
 #  Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
